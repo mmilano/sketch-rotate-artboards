a0a022de75a54acc61361139534c941a82a10838
Michael Dziedzic <michaeldzie@gmail.com>
2017-02-21 15:57:48 -0800

    Refactor

--- rotate-artboards.sketchplugin/Contents/Sketch/rotate-artboard-orientation.js
+++ rotate-artboards.sketchplugin/Contents/Sketch/rotate-artboard-orientation.js
@@ -19,7 +19,7 @@ function onRun(context) {
 
     if (selectedCount == 0) {
         log('Nothing selected.');
-    } 
+    }
     else {
         log('Selected items: \n');
         for (var i = 0; i < selectedCount; i++) {
@@ -33,20 +33,18 @@ function onRun(context) {
                 var artboard = item;
                 var existing_width = artboard.frame().width();
                 var existing_height = artboard.frame().height();
-                
+
                 // flip the orientation
                 var new_width = existing_height;
                 var new_height = existing_width;
                 artboard.frame().setWidth(new_width);
                 artboard.frame().setHeight(new_height);
-                
+
                 log ("Changed orientation of artboard: " + item.name());
                 log ("old dimensions: " + existing_width +"," + existing_height);
-                log ("new dimensions: " + new_width +"," + new_height);           
+                log ("new dimensions: " + new_width + "," + new_height);           
             };
         log ("\r");
         };
     };
 };
-
-
