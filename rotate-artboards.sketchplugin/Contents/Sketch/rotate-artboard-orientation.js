ca4c7896a31e4bde1671e0009461de29565ed3df
Michel Milano <mmilano+git@gmail.com>
2017-02-21 13:50:24 -0800

    add initial plugin source

---
+++ rotate-artboards.sketchplugin/Contents/Sketch/rotate-artboard-orientation.js
@@ -0,0 +1,52 @@
+// Rotate Artboard Orientation
+// author: Michel Milano
+// — Source code available at [GitHub](https://github.com/???/plugins.rotate-artboard)
+//
+// Rotates the currently selected artboard(s)_' orientation, portrait to landscape -
+// width becomes the height, height becomes the width.
+
+
+var APP = NSApplication.sharedApplication;
+
+function onRun(context) {
+
+  var Sketch = context.api()
+
+    var selectedItems = context.selection;
+    var selectedCount = selectedItems.count();
+
+    var artboardsWereSelected = false;
+
+    if (selectedCount == 0) {
+        log('Nothing selected.');
+    } 
+    else {
+        log('Selected items: \n');
+        for (var i = 0; i < selectedCount; i++) {
+            var item = selection[i];
+
+            if (item.className() != 'MSArtboardGroup') {
+                log ("selection #" +(i+1)+ " is not an artboard");
+            } else {
+                log ("selection #" +(i+1)+ " is an artboard");
+
+                var artboard = item;
+
+                var existing_width = artboard.frame().width();
+                var existing_height = artboard.frame().height();
+                
+                // flip the orientation
+                var new_width = existing_height;
+                var new_height = existing_width;
+                artboard.frame().setWidth(new_width);
+                artboard.frame().setHeight(new_height);
+                
+                log ("changed orientation of artboard: " + item.name());
+                log ("old dimensions: " + existing_width +"," + existing_height);
+                log ("new dimensions: " + new_width +"," + new_height);           
+            };
+        log ("\r");
+        };
+    };
+};
+
